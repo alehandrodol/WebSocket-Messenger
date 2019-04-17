a95cbfea8b0de7a3043a77a97ab91b1db3fa37a0
alehandrodol <alexeydol2002@mail.ru>
2019-04-17 23:18:04 +0300

    Добавлено сохранение сообщений в БД!

---
+++ main.py
@@ -0,0 +1,54 @@
+import asyncio
+import datetime
+import websockets
+import mysql.connector
+from mysql.connector import MySQLConnection, Error
+from python_mysql_dbconfig import read_db_config
+
+def connect():
+    """ Connect to MySQL database """
+
+    db_config = read_db_config()
+
+    try:
+        print('Connecting to MySQL database...')
+        conn = MySQLConnection(**db_config)
+
+        if conn.is_connected():
+            print('connection established.')
+        else:
+            print('connection failed.')
+
+    except Error as error:
+        print(error)
+
+
+clients = []
+connect()
+
+
+async def ws(websocket, path):
+    dbconfig = read_db_config()
+    conn = MySQLConnection(**dbconfig)
+    cursor = conn.cursor()
+    nickname = await websocket.recv()
+    clients.append({"ws": websocket, "nick": nickname})
+    current_idx = len(clients) - 1
+    print(f"New connection {current_idx}")
+    while True:
+        message = await websocket.recv()
+
+        query = "INSERT INTO messages (Login, m_Text) VALUES (%s,%s)"
+        args = (clients[current_idx].get('nick'), message)
+        cursor.execute(query, args)
+        conn.commit()
+
+        to_send = f"{datetime.datetime.now().strftime('%H:%M:%S')} | {clients[current_idx].get('nick')}: {message}"
+
+        for client in clients:
+            await client.get('ws').send(to_send)
+        await asyncio.sleep(0)
+
+start_server = websockets.serve(ws, "localhost", 8081)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
